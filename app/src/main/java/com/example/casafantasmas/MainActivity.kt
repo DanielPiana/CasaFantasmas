1f46c19cbbccdd3a01c5e6a2ca3add4d7b000b0f
Pepitoelgringo <kxzdanidp@gmail.com>
2024-11-28 15:33:25 +0100

    Comentarios añadidos

--- app/src/main/java/com/example/casafantasmas/MainActivity.kt
+++ app/src/main/java/com/example/casafantasmas/MainActivity.kt
@@ -39,41 +39,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun assingCardsToArray() {
-        for(row in 0 until 4) {
-            for (column in 0 until 4) {
-                val index = row * 4 + column
-                val cardView = gridLayout.getChildAt(index) as CardView
-                board[row][column] = cardView
-                Log.d("CardAssignment", "board[$row][$column] = $cardView")
+        for(row in 0 until 4) { //For para las filas
+            for (column in 0 until 4) { //For para las columnas
+                val index = row * 4 + column // con row * 4 accedemos a la columna y al sumarle la columna, accedemos a la fila (un indice del
+                val cardView = gridLayout.getChildAt(index) as CardView //Accedes a la cardview de esa posicion con el index
+                board[row][column] = cardView //Y la guardamos en la posicion del array
+                Log.d("CardAssignment", "board[$row][$column] = $cardView")//Log para comprobación
             }
         }
     }
 
     private fun initGridLayout() {
-        for (i in 1..16) {
+        for (i in 1..16) { //For para recorrer el gridLayout
+            //Creamos la cardView
             cardView = LayoutInflater.from(this).inflate(R.layout.card, gridLayout, false) as CardView
-
+            //Creamos la imageView de dentro de la tarjeta
             imageView = cardView.findViewById(R.id.imageView)
-
+            //Seteamos el fondo predeterminado de las tarjetas
             imageView.setImageResource(R.drawable.ic_launcher_background)
-
+            //Añadimos la vista de la tarjeta al gridLayout
             gridLayout.addView(cardView)
         }
         initStartFinish()
     }
 
     private fun initStartFinish() {
-
+        //Con un numero aleatorio, lo utilizamos para acceder a la tarjeta de una posicion aleatoria en el gridlayout
         val cardStart = gridLayout.getChildAt(randomCardStart) as CardView
+        //con la tarjeta ya podemos inicializar la imagen y poder cambiar el fondo de una, que será el comienzo
         val imageStart = cardStart.findViewById<ImageView>(R.id.imageView)
         imageStart.setImageResource(R.drawable.img_1)
 
+        //Hacmeos lo mismo con otra posicion para el final
         val cardFinish = gridLayout.getChildAt(randomCardFinish) as CardView
         val imageFinish = cardFinish.findViewById<ImageView>(R.id.imageView)
         imageFinish.setImageResource(R.drawable.img)
     }
 
-    private fun initComponents() {
+    private fun initComponents() {//Metodo para inicializar componentes
         mibinding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(mibinding.root)
         gridLayout = mibinding.gridLayout
