0b7a77db24f71a3ae359b40af55fca3d74088150
danielpiana <kxzdani@gmail.com>
2024-11-28 11:46:20 +0100

    Asignada cada posicion del gridview a un array bidimensional

--- app/src/main/java/com/example/casafantasmas/MainActivity.kt
+++ app/src/main/java/com/example/casafantasmas/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.casafantasmas
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.GridLayout
 import android.widget.ImageView
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var gridLayout: GridLayout
     lateinit var cardView: CardView
     lateinit var imageView: ImageView
+    val randomCardStart: Int = (0..15).random()
+    val randomCardFinish: Int = (0..15).random()
+    val board = Array(4) { arrayOfNulls<CardView>(4) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +28,29 @@ class MainActivity : AppCompatActivity() {
 
         initComponents()
         initGridLayout()
+        assingCardsToArray()
 
         //Acceder a una tarjeta en concreto
         val card1 = gridLayout.getChildAt(0) as CardView
         //Acceder a la imagen de dentro de la tarjeta
         val image1 = card1.findViewById<ImageView>(R.id.imageView)
 
+
+    }
+
+    private fun assingCardsToArray() {
+        for(row in 0 until 4) {
+            for (column in 0 until 4) {
+                val index = row * 4 + column
+                val cardView = gridLayout.getChildAt(index) as CardView
+                board[row][column] = cardView
+                Log.d("CardAssignment", "board[$row][$column] = $cardView")
+            }
+        }
     }
 
     private fun initGridLayout() {
         for (i in 1..16) {
-
             cardView = LayoutInflater.from(this).inflate(R.layout.card, gridLayout, false) as CardView
 
             imageView = cardView.findViewById(R.id.imageView)
@@ -45,14 +61,13 @@ class MainActivity : AppCompatActivity() {
         }
         initStartFinish()
     }
+
     private fun initStartFinish() {
-        val randomCardStart: Int = (0..15).random()
 
         val cardStart = gridLayout.getChildAt(randomCardStart) as CardView
         val imageStart = cardStart.findViewById<ImageView>(R.id.imageView)
         imageStart.setImageResource(R.drawable.img_1)
 
-        val randomCardFinish: Int = (0..15).random()
         val cardFinish = gridLayout.getChildAt(randomCardFinish) as CardView
         val imageFinish = cardFinish.findViewById<ImageView>(R.id.imageView)
         imageFinish.setImageResource(R.drawable.img)
