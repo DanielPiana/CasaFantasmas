de89a89f5a3a132f46e12d3644d647bbf3c0d87a
danielpiana <kxzdani@gmail.com>
2024-11-28 10:41:22 +0100

    Puesto un imageView dentro de las card, hecho para elegir aleatoriamente el comienzo y el final, puesto fondo predeterminado en el resto de casillas

--- app/src/main/java/com/example/casafantasmas/MainActivity.kt
+++ app/src/main/java/com/example/casafantasmas/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.casafantasmas
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.GridLayout
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.example.casafantasmas.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var mibinding: ActivityMainBinding
     lateinit var gridLayout: GridLayout
     lateinit var cardView: CardView
-    lateinit var textView: TextView
+    lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +27,35 @@ class MainActivity : AppCompatActivity() {
 
         //Acceder a una tarjeta en concreto
         val card1 = gridLayout.getChildAt(0) as CardView
-        //Acceder al texto de dentro de la tarjeta
-        val text1 = card1.findViewById<TextView>(R.id.textView)
-
-
-
+        //Acceder a la imagen de dentro de la tarjeta
+        val image1 = card1.findViewById<ImageView>(R.id.imageView)
 
     }
 
     private fun initGridLayout() {
         for (i in 1..16) {
+
             cardView = LayoutInflater.from(this).inflate(R.layout.card, gridLayout, false) as CardView
-            textView = cardView.findViewById<TextView>(R.id.textView)
-            textView.text = i.toString()
+
+            imageView = cardView.findViewById(R.id.imageView)
+
+            imageView.setImageResource(R.drawable.ic_launcher_background)
+
             gridLayout.addView(cardView)
         }
+        initStartFinish()
+    }
+    private fun initStartFinish() {
+        val randomCardStart: Int = (0..15).random()
+
+        val cardStart = gridLayout.getChildAt(randomCardStart) as CardView
+        val imageStart = cardStart.findViewById<ImageView>(R.id.imageView)
+        imageStart.setImageResource(R.drawable.img_1)
+
+        val randomCardFinish: Int = (0..15).random()
+        val cardFinish = gridLayout.getChildAt(randomCardFinish) as CardView
+        val imageFinish = cardFinish.findViewById<ImageView>(R.id.imageView)
+        imageFinish.setImageResource(R.drawable.img)
     }
 
     private fun initComponents() {
